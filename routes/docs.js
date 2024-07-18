a2aa23b4e9242b145cf008f483fceda853031e98
ThiagoAndo <ando.thiago@gmail.com>
2024-07-18 12:07:51 +0100

    Testing nav links

--- routes/docs.js
+++ routes/docs.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/doc", (req, res) => {
+router.get("/", (req, res) => {
   res.render("index.html");
 });
 
+router.get("/prt", (req, res) => {
+  res.render("products.html");
+});
 
 module.exports = router;
