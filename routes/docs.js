9b1e0d85487a1d94359df318f887f16404c2b7ff
ThiagoAndo <ando.thiago@gmail.com>
2024-07-19 13:05:19 +0100

    include html files

--- routes/docs.js
+++ routes/docs.js
@@ -9,19 +9,19 @@ router.get("/prt", (req, res) => {
   res.render("products.html");
 });
 router.get("/user", (req, res) => {
-  res.render("products.html");
+  res.render("user.html");
 });
 
 router.get("/cart", (req, res) => {
-  res.render("products.html");
+  res.render("cart.html");
 });
 
 router.get("/address", (req, res) => {
-  res.render("products.html");
+  res.render("address.html");
 });
 
 router.get("/order", (req, res) => {
-  res.render("products.html");
+  res.render("order.html");
 });
 
 router.get("/c", (req, res) => {
